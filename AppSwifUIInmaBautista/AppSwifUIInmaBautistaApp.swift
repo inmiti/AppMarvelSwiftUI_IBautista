7ca52545e18dab50e19401a66d7d96a47116976f
inmiti <inmabauvel@gmail.com>
2023-11-16 12:51:19 +0100

    Initial Commit

---
+++ AppSwifUIInmaBautista/AppSwifUIInmaBautistaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSwifUIInmaBautistaApp.swift
+//  AppSwifUIInmaBautista
+//
+//  Created by ibautista on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AppSwifUIInmaBautistaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
